a608569cc259c4cff794158b497300e13d0f45eb
Varun Naidu <n.varunnaidu@gmail.com>
2018-07-10 09:21:42 +0530

    fixed linting

--- index.js
+++ index.js
@@ -5,3 +5,21 @@
  * in english language to that in the equivalent braille
  * code.
  */
+import englishToBrailleLiteralSet from './english-to-braille.js';
+
+function response() {
+  const inputstring = document.getElementById('sourceLangText').value;
+  let output = '';
+  for (let i = 0; i < inputstring.length; i += 1) {
+    for (let x = 0; x < englishToBrailleLiteralSet.length; x += 1) {
+      if (inputstring[i] === englishToBrailleLiteralSet[x][0]) {
+        output += englishToBrailleLiteralSet[x][1];
+      }
+    }
+  }
+  document.getElementById('targetLangText').innerHTML = output;
+}
+
+window.onload = function () {
+  document.getElementById('btnConvertEnglishToBraille').addEventListener('click', response);
+};
